a34aa191c15dcb9557a12106c8efa7c114aa728c
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-22 20:40:45 +0200

    ReferenceLoopHandling

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// 
+
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(o=>
+    o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
